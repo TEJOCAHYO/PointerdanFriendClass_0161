127a4c8bd733a6a97ed180d2464b3680f56909f9
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-05-05 16:31:21 +0700

    membuat labrary

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -1,26 +1,3 @@
 #incule <iostream>
-using namespace std:
+using namespace std;
 
-Class mahasiswa {
-    public:
-    int nim;
-    void ShowNim(){
-        cout<<"No Induk = "<<nim<<endl;
-    }
-};
-
-int main(){
-    
-    Mahasiswa mhs; // Object mhs
-    mhs.nim = 5;
-    mhs showNim(); // Memeber Access Operator
-
-    mahasiswa &refMhs = mhs; //pointer Reference refMhs
-    refMhs.nim = 2; // Member Access Operator
-    mhs.showNim();
-
-    Mahasiswa *pMhs = &Mhs; //pointer Dereference pMhs
-    pMhs->nim = 3; // Arrow Operator
-    pMhs->showNim();
-    return 0;
-}
