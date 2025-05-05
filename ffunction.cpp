463c8e8cdeac92582757705eeea7e75c64c2c3a0
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-05-05 16:29:51 +0700

    alamat memori

--- ffunction.cpp
+++ ffunction.cpp
@@ -33,3 +33,10 @@ int main(){
     *c = 9;
     cout << endl;
     cout << a << endl;
+    cout << "alamat memori a = " << &a << endl;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = " << *c << endl;
+    c = &b;
+    cout << "alamat memori c = " << c << endl;
+    cout << "cetak c = "  << *c << endl; 
+}
